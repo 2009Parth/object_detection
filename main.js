5356e152675871ae1866af30163e613ab9432b7f
2009Parth <111286031+2009Parth@users.noreply.github.com>
2023-06-06 10:15:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function next1()
+{
+    window.location = "bed_room.html";
+}
+function next2()
+{
+    window.location = "tv_&_ac.html";
+}
+function next3()
+{
+    window.location = "desk.html";
+}
+function next4()
+{
+    window.location = "bottles.html";
+}
+function next5()
+{
+    window.location = "fruit.html";
+}
